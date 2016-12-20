4c25dd4c87d1202d5a5e9b5c75947f34353c3122
Emmanuel Krebs <e.prhbs@gmail.com>
2016-12-20 17:00:31 +0100

    animation on article being removed (v0.11)

--- chrome/app/list/app.js
+++ chrome/app/list/app.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-unused-vars */
 const browser = chrome;
-const app = angular.module('pileList', ['angular.snackbar']);
+const app = angular.module('pileList', ['angular.snackbar', 'ngAnimate']);
 /* eslint-enable no-unused-vars */
