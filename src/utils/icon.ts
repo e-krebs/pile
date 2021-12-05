d62a46c8d7840d8dacd78ece6addcfa3ade98240
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2021-12-05 14:33:44 +0100

    add excludedIcons management

--- src/utils/icon.ts
+++ src/utils/icon.ts
@@ -3,9 +3,11 @@ import Vibrant from 'node-vibrant';
 import { readFile, readJson, writeBlob, writeJson, deleteFolder } from 'utils/files';
 import { getBlob } from 'utils/get';
 import type { BlobInfo, Path, Response } from 'utils/typings';
+import { getTimestamp, isCacheExpired, JsonCache } from './dataCache';
 import { Palette, resolvePalette } from './palette';
 
 const iconFolder = 'icons';
+const noIconsFile: Path = [iconFolder, 'noIcons.json'];
 
 const getIconBlob = async (
   hostname: string,
@@ -47,6 +49,38 @@ const getPalette = async (url: string, palettePath: Path): Promise<IconAndPalett
   });
 };
 
+type ExcludedIcon = JsonCache<string>;
+
+const clearExcludedIcons = async (icons: ExcludedIcon[] = []): Promise<ExcludedIcon[]> => {
+  const cleared = [];
+  let updated = false;
+  for (const icon of icons) {
+    if (!isCacheExpired(icon)) {
+      cleared.push(icon);
+      updated = true;
+    }
+  }
+  if (updated) {
+    await writeJson(noIconsFile, cleared);
+  }
+  return cleared;
+};
+
+const addExcludedIcon = async (hostname: string): Promise<void> => {
+  const excludedIcons = (await readJson<ExcludedIcon[]>(noIconsFile)) ?? [];
+  if (excludedIcons.map(icon => icon.data).includes(hostname)) return;
+  const newList = await clearExcludedIcons(excludedIcons);
+  await writeJson<ExcludedIcon[]>(
+    noIconsFile,
+    [...newList, { timestamp: getTimestamp(), data: hostname }]
+  );
+};
+
+const getExcludedIcons = async (): Promise<string[]> => {
+  const excludedIcons = (await readJson<ExcludedIcon[]>(noIconsFile)) ?? [];
+  return (await clearExcludedIcons(excludedIcons)).map(icon => icon.data);
+};
+
 export const getIcon = async (
   hostname: string,
   fallback: string | null = null
@@ -54,12 +88,18 @@ export const getIcon = async (
   const imageName: Path = [iconFolder, `${hostname}.png`];
   const paletteName: Path = [iconFolder, `${hostname}_palette.json`];
 
+  const excludedIcons = await getExcludedIcons();
+  if (excludedIcons.includes(hostname)) return;
+
   try {
     let imageUrl = await readFile(imageName);
     if (imageUrl !== null) return getPalette(imageUrl, paletteName);
 
     const res = await getIconBlob(hostname, fallback);
-    if (!res.ok) return;
+    if (!res.ok) {
+      await addExcludedIcon(hostname);
+      return;
+    }
 
     imageUrl = await writeBlob({ blob: res.result.blob, blobPath: imageName });
     if (imageUrl === null) return;
