36aaa4697f2e52d4416b788597c4bc30992dbc07
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2021-12-22 16:02:04 +0100

    add Tags component

--- src/components/List/Item/Tags.tsx
+++ src/components/List/Item/Tags.tsx
@@ -1,33 +1,92 @@
 import cx from 'classnames';
-import { FC, useMemo } from 'react';
-import { Tag } from 'react-feather';
+import { FC, useMemo, useState } from 'react';
+import { Loader, Plus, Tag, X, XCircle } from 'react-feather';
+import { useQueryClient } from 'react-query';
 
+import { useService } from 'hooks';
 import { getRgba } from 'utils/palette';
+import { clearCache } from 'utils/dataCache';
 import { useItemContext } from './ItemContext';
+import { TagAutocomplete } from './TagAutocomplete';
+import { TagsContext } from './TagsContext';
 
 export const Tags: FC = () => {
-  const { rgb, tags, isOpen } = useItemContext();
+  const { getQueryKey, removeTag } = useService();
+  const { id, rgb, tags, isOpen, isAddTagsOpen, setIsAddTagsOpen } = useItemContext();
+  const queryClient = useQueryClient();
+  const [isLoading, setIsLoading] = useState(false);
+
   const color = useMemo(() => getRgba(rgb, 0.8), [rgb]);
   const borderColor = useMemo(() => getRgba(rgb, 0.6), [rgb]);
+  const TagIcon = useMemo(() => isLoading ? Loader : Tag, [isLoading]);
+  const Add = useMemo(() => isAddTagsOpen ? XCircle : Plus, [isAddTagsOpen]);
+
+  const remove = async (tag: string) => {
+    setIsLoading(true);
+    const ok = await removeTag(id, tag);
+    if (ok) {
+      await clearCache(getQueryKey, queryClient);
+    }
+    setIsLoading(false);
+  };
 
   return (
-    <div
-      className="text-xs leading-3 rounded-full cursor-default"
-      style={{ color, borderColor }}
-    >
+    <TagsContext.Provider value={{ isLoading, setIsLoading }}>
       <div
         className={cx(
-          'flex items-center px-2 py-1 h-fit border rounded-full bg-gray-100',
-          isOpen ? 'border-inherit text-inherit' : 'border-gray-400 text-gray-500'
+          'group text-xs leading-4 rounded-lg cursor-default flex flex-col',
+          isAddTagsOpen ? 'max-w-[calc(50%+1rem)]' : 'max-w-[25%]',
+          'hover:max-w-[calc(50%+1rem)] transition-max-width'
         )}
+        style={{ color, borderColor }}
       >
-        <Tag className="w-3 h-3 mb-[-1px] shrink-0" />
-        {tags.length > 0 && (
-          <div className="truncate mb-px mt-[-1px]">
-            {tags.map(tag => <span key={tag} className="ml-1" title={tag}>{tag}</span>)}
+        <div
+          className={cx(
+            'flex items-center px-2 py-1 border rounded-lg bg-gray-100',
+            isOpen ? 'border-inherit text-inherit' : 'border-gray-400 text-gray-500'
+          )}
+        >
+          <TagIcon
+            className={cx('w-3 h-3 mt-[3px] mb-[2px] shrink-0', isLoading && 'animate-spin')}
+          />
+
+          {tags.length > 0 && (
+            <div className="flex truncate mt-[-1px]">
+              {tags.map(tag => (
+                <div
+                  key={tag}
+                  className={cx(
+                    'ml-1 pl-px flex items-center group',
+                    'border-b border-transparent',
+                    !isLoading && 'cursor-pointer border-dashed hover:border-inherit',
+                  )}
+                  title={`remove tag [${tag}]`}
+                  onClick={() => isLoading ? {} : remove(tag)}
+                >
+                  <span>{tag}</span>
+                  <X className={cx(
+                    'group-hover:w-3 transition-width h-3 mb-[-3px]',
+                    isAddTagsOpen ? 'w-3' : 'w-0',
+                  )} />
+                </div>
+              ))}
+            </div>
+          )}
+
+          <div
+            className={cx('shrink-0 grow flex justify-end', !isLoading && 'cursor-pointer')}
+            title={isAddTagsOpen ? 'close' : 'add tags'}
+            onClick={() => isLoading ? {} : setIsAddTagsOpen(!isAddTagsOpen)}
+          >
+            <Add className={cx(
+              isAddTagsOpen ? 'w-3' : 'w-0',
+              'group-hover:w-3 transition-width h-3 mb-[-1px] group-hover:ml-1'
+            )} />
           </div>
-        )}
-      </div>
-    </div >
+        </div>
+
+        {isAddTagsOpen && <TagAutocomplete />}
+      </div >
+    </TagsContext.Provider>
   );
 };
