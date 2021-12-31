80de97123f81f4fa72da495e70d3525c78980889
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2021-12-31 12:15:19 +0100

    update google url to the redirected one

--- src/utils/icon.ts
+++ src/utils/icon.ts
@@ -13,7 +13,7 @@ const getIconBlob = async (
   fallback: string | null
 ): Promise<Response<BlobInfo>> => {
   let res = await getBlob({
-    url: `https://www.google.com/s2/favicons?domain=${hostname}&alt=404&size=32`
+    url: `https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&url=${hostname}&size=32`
   });
   if (res.ok) return res;
 
