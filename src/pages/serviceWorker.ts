6bcb1cbcc449d79bcfed45764a61c4b722128e27
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2022-05-07 15:26:34 +0200

    service worker: contextMenu click + content script menu message

--- src/pages/serviceWorker.ts
+++ src/pages/serviceWorker.ts
@@ -1,9 +1,10 @@
 import { vars, defaultVars } from 'helpers/vars';
-import { getServices } from 'utils/services';
+import { getService, getServices } from 'utils/services';
 import { setBadge, setBadgeColor } from 'utils/badge';
 import { forceGet } from 'utils/get';
 import { currentUrlIsMatching } from 'utils/currentUrlIsMatching';
 import { createContextMenus } from 'utils/createContextMenus';
+import { Message } from 'utils/messages';
 
 const { refreshInterval } = vars;
 const { refreshInterval: defaultRefreshInterval } = defaultVars;
@@ -55,9 +56,64 @@ const tabsActivatedListener = async ({ tabId }: chrome.tabs.TabActiveInfo) => {
 
 const onInstalledListener = async () => await createContextMenus();
 
+const onMessageListener = async (message: Message, _: unknown, sendMessage: () => void) => {
+  switch (message.action) {
+    case 'addToService': {
+      sendMessage();
+      const service = getService(message.service);
+      if (!service) {
+        throw Error(`couldn't find service "${message.service}"`);
+      }
+      await service.add(message.url);
+      await refreshBadge();
+      return;
+    }
+    case 'archiveFromService': {
+      sendMessage();
+      const service = getService(message.service);
+      if (!service) {
+        throw Error(`couldn't find service "${message.service}"`);
+      }
+      await service.archiveItem(message.id);
+      await refreshBadge();
+      return;
+    }
+    case 'deleteFromService': {
+      sendMessage();
+      const service = getService(message.service);
+      if (!service) {
+        throw Error(`couldn't find service "${message.service}"`);
+      }
+      await service.deleteItem(message.id);
+      await refreshBadge();
+      return;
+    }
+  }
+};
+
+const onContextMenuClickedListener = (info: chrome.contextMenus.OnClickData, tab?: chrome.tabs.Tab) => {
+  if (!tab?.url) {
+    throw Error('no tab.url on contextMenu clicked');
+  }
+  if (!tab?.id) {
+    throw Error('no tab.id on contextMenu clicked');
+  }
+  const service = getService(info.menuItemId.toString());
+  if (!service) {
+    throw Error(`couldn't find service "${info.menuItemId}"`);
+  }
+  const message: Message = {
+    action: 'service',
+    service: service.name,
+    url: tab.url,
+  };
+  chrome.tabs.sendMessage(tab.id, message);
+};
+
 chrome.tabs.onUpdated.addListener(tabsUpdatedListener);
 chrome.tabs.onActivated.addListener(tabsActivatedListener);
 chrome.runtime.onInstalled.addListener(onInstalledListener);
+chrome.runtime.onMessage.addListener(onMessageListener);
 chrome.contextMenus.onClicked.addListener(onContextMenuClickedListener);
 
 (async () => {
