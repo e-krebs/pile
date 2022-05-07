f5cbbc023a09bd6fd18f4bcfc9a11af3cbeaf517
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2022-05-07 15:10:58 +0200

    add createContextMenus helper

---
+++ src/utils/createContextMenus.ts
@@ -0,0 +1,21 @@
+import { getServices } from './services';
+
+export const createContextMenus = async () => {
+  await new Promise((resolve) => {
+    chrome.contextMenus.removeAll(() => {
+      resolve(true);
+    });
+  });
+  await Promise.all(
+    getServices().map(async (service) => {
+      const isConnected = await service.isConnected();
+      if (!isConnected) return;
+      chrome.contextMenus.create({
+        id: service.name,
+        title: `pile for ${service.name}`,
+        type: 'normal',
+        contexts: ['page'],
+      });
+    })
+  );
+};
