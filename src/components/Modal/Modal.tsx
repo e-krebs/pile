66c3b9bb9afd765c3d2de29200b265e2cd6c1985
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2022-05-08 14:29:02 +0200

    feat: Modal & ModalContext

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,43 @@
+import cx from 'classnames';
+import { FC, HTMLAttributes, PropsWithChildren } from 'react';
+import { X } from 'react-feather';
+
+import { useModal } from './ModalContext';
+
+interface ModalProps extends HTMLAttributes<HTMLDivElement> {
+  title: string;
+  contentProps?: HTMLAttributes<HTMLElement>;
+}
+
+export const Modal: FC<PropsWithChildren<ModalProps>> = ({
+  title,
+  children,
+  contentProps,
+  ...props
+}) => {
+  const { closing, closeModal } = useModal();
+
+  return (
+    <div
+      {...props}
+      className="fixed top-0 left-0 z-[9999] flex h-screen w-screen items-start justify-center bg-black bg-opacity-70 pt-[33vh] text-white dark:bg-white dark:bg-opacity-70"
+      onClick={closeModal}
+    >
+      <section
+        {...contentProps}
+        className={cx(
+          'relative grid w-64 animate-spin gap-4 rounded-lg p-4',
+          closing ? 'animate-blowDownModal' : 'animate-blowUpModal'
+        )}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <X
+          className="absolute right-0 h-8 w-8 cursor-pointer rounded-full p-1 hover:bg-gray-200 dark:hover:bg-gray-700"
+          onClick={closeModal}
+        />
+        <h1 className="px-8 pt-3 pb-0 text-center text-xl font-bold capitalize">{title}</h1>
+        {children}
+      </section>
+    </div>
+  );
+};
