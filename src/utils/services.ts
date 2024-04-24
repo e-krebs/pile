fccde8568fad27e4c710963fd0d229f01f16dcbe
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2024-04-24 16:11:47 +0200

    chore: update Service definition - hasOAuth - isUpdatable

--- src/utils/services.ts
+++ src/utils/services.ts
@@ -4,22 +4,38 @@ import { ServiceNames, services } from 'services';
 import { GetParams } from './get';
 import { ListItem } from './typings';
 
-export interface Service {
+interface BaseService {
   name: ServiceNames;
   getQueryKey: string;
   get: (param: GetParams) => Promise<ListItem[]>;
-  add: (url: string, tags?: string[]) => Promise<void>;
-  authorize: () => Promise<boolean>;
-  connect: () => Promise<boolean>;
   disconnect: () => Promise<void | void[]>;
   isConnected: () => Promise<boolean>;
+  Icon: FC<{ className?: string }>;
+  borderClassName: string;
+  isTogglable: boolean;
+}
+
+export interface UpdatableService {
+  add: (url: string, tags?: string[]) => Promise<void>;
   archiveItem: (id: string) => Promise<boolean>;
   deleteItem: (id: string) => Promise<boolean>;
   addTag: (id: string, tag: string) => Promise<boolean>;
   removeTag: (id: string, tag: string) => Promise<boolean>;
-  Icon: FC<{ className?: string }>;
-  borderClassName: string;
 }
+
+export interface ServiceWithOAuth {
+  authorize: () => Promise<boolean>;
+  connect: () => Promise<boolean>;
+}
+
+export type ComponentContext = 'popup' | 'options';
+export interface ServiceWithoutOAuth {
+  Setup: FC<{ context: ComponentContext }>;
+}
+
+export type Service = BaseService &
+  (({ hasOAuth: true } & ServiceWithOAuth) | ({ hasOAuth: false } & ServiceWithoutOAuth)) &
+  (({ isUpdatable: true } & UpdatableService) | { isUpdatable: false });
 
 export const getServices = () => {
   return Object.values(services) as Service[];
