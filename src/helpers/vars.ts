de91502a7b26ed095fbc115a99e4b03307a27d57
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2024-04-24 16:31:37 +0200

    chore: add lastTag utils & remember last tab

--- src/helpers/vars.ts
+++ src/helpers/vars.ts
@@ -1,7 +1,12 @@
 export type VarKeys = 'refreshInterval';
+export type ServiceVarKeys = 'active';
 
 export const vars: Record<VarKeys, string> = {
   refreshInterval: 'refresh_interval',
+};
+
+export const serviceVars: Record<ServiceVarKeys, string> = {
+  active: 'service_active',
 };
 
 export const defaultVars: Record<VarKeys, number> = {
