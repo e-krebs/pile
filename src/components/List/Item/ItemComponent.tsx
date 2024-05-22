14dec2af825cb1974f4b68201b6d9cc1492fa928
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2024-05-22 15:56:11 +0200

    chore: ItemComponent forwardRef

--- src/components/List/Item/ItemComponent.tsx
+++ src/components/List/Item/ItemComponent.tsx
@@ -1,5 +1,5 @@
 import cx from 'classnames';
-import type { FC } from 'react';
+import { RefAttributes, forwardRef, type FC } from 'react';
 
 import { getRgba } from 'utils/palette';
 import { beautifyUrl } from 'utils/url';
@@ -12,41 +12,45 @@ import { ArchiveAction } from './ArchiveAction';
 import { Tags } from './Tags';
 import { useService } from 'hooks';
 
-export const ItemComponent: FC = () => {
-  const { isUpdatable } = useService();
-  const { url, title, rgb, isOpen } = useItemContext();
+export const ItemComponent: FC<RefAttributes<HTMLDivElement>> = forwardRef<HTMLDivElement>(
+  function ItemComponent(_, ref) {
+    const { isUpdatable } = useService();
+    const { url, title, rgb, isOpen } = useItemContext();
 
-  return (
-    <div className="rounded-md" style={{ backgroundColor: getRgba(rgb, 0.1) }}>
-      <div
-        className={cx(
-          'rounded-md py-2 hover:bg-inherit',
-          isOpen ? 'bg-inherit' : 'bg-white dark:bg-gray-900'
-        )}
-      >
-        <div className="flex items-start space-x-4 pl-2">
-          <MainIcon />
-          <div className={cx('flex grow items-baseline space-x-2 overflow-auto')}>
-            <Link
-              className={cx('grow pt-0.5 hover:text-inherit', !isOpen && 'truncate')}
-              url={url}
-              title={`${beautifyUrl(url)} – ${title}`}
+    return (
+      <div ref={ref} className="rounded-md" style={{ backgroundColor: getRgba(rgb, 0.1) }}>
+        <div
+          className={cx(
+            'rounded-md py-2 hover:bg-inherit',
+            isOpen ? 'bg-inherit' : 'bg-white dark:bg-gray-900'
+          )}
+        >
+          <div className="flex items-start space-x-4 pl-2">
+            <MainIcon />
+            <div className={cx('flex grow items-baseline space-x-2 overflow-auto')}>
+              <Link
+                className={cx('grow pt-0.5 hover:text-inherit', !isOpen && 'truncate')}
+                url={url}
+                title={`${beautifyUrl(url)} – ${title}`}
+              >
+                {title}
+              </Link>
+              <Tags />
+            </div>
+            <Chevron />
+          </div>
+          {isUpdatable && (
+            <div
+              className={cx('flex px-2 transition-height', isOpen ? 'visible h-10' : 'invisible h-0')}
             >
-              {title}
-            </Link>
-            <Tags />
-          </div>
-          <Chevron />
+              <div className="grow" />
+              <DeleteAction />
+              <ArchiveAction />
+              <div className="h-8 w-8 shrink-0" />
+            </div>
+          )}
         </div>
-        {isUpdatable && (
-          <div className={cx('flex px-2 transition-height', isOpen ? 'visible h-10' : 'invisible h-0')}>
-            <div className="grow" />
-            <DeleteAction />
-            <ArchiveAction />
-            <div className="h-8 w-8 shrink-0" />
-          </div>
-        )}
       </div>
-    </div>
-  );
-};
+    );
+  }
+);
