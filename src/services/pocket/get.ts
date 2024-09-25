98e242d17d63de04def0871f019da09875ab249c
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2024-09-25 12:01:31 +0200

    fix: pocket API is actively requesting pagination (and state) now (#171)

--- src/services/pocket/get.ts
+++ src/services/pocket/get.ts
@@ -8,25 +8,46 @@ import { itemToListItem, PocketItem } from './item';
 
 interface PocketList {
   list: Record<number, PocketItem>;
+  total: string;
 }
 
+// limit from the doc
+const count = 30;
+
 export const get = async (param: GetParams): Promise<ListItem[]> => {
-  const response = await post<PocketList>({
-    url: 'https://getpocket.com/v3/get',
-    headers,
-    params: {
-      consumer_key: getPocketKey(),
-      access_token: await getPocketToken(),
-      sort: 'newest',
-      search: param.type === 'search' ? param.search : undefined,
-      tag: param.type === 'tag' ? param.tag ?? '_untagged_' : undefined,
-      detailType: 'complete',
-    },
-  });
+  const listItems: ListItem[] = [];
+  let offset = 0;
+  let total = 0;
 
-  if (!response.ok) throw Error("couldn't get pocket list");
+  do {
+    const response = await post<PocketList>({
+      url: 'https://getpocket.com/v3/get',
+      headers,
+      params: {
+        consumer_key: getPocketKey(),
+        access_token: await getPocketToken(),
+        sort: 'newest',
+        search: param.type === 'search' ? param.search : undefined,
+        tag: param.type === 'tag' ? param.tag ?? '_untagged_' : undefined,
+        detailType: 'complete',
+        state: 'unread',
+        count,
+        total: '1',
+        offset,
+      },
+    });
 
-  return Object.values(response.result.list)
-    .sort((a, b) => (a.time_added < b.time_added ? 1 : -1))
-    .map(itemToListItem);
+    if (!response.ok) throw Error("couldn't get pocket list");
+
+    listItems.push(
+      ...Object.values(response.result.list)
+        .sort((a, b) => (a.time_added < b.time_added ? 1 : -1))
+        .map(itemToListItem)
+    );
+
+    total = parseInt(response.result.total);
+    offset += count;
+  } while (listItems.length < total);
+
+  return listItems;
 };
