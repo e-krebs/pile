4441831a60ca26da39408ff14594ed09ab26a781
Emmanuel Krebs <e-krebs@users.noreply.github.com>
2024-11-27 10:32:34 +0100

    fix: remove workaround

--- vite.config.ts
+++ vite.config.ts
@@ -7,19 +7,10 @@ import { resolve } from 'path';
 import manifestJson from './manifest.json';
 
 const manifest = manifestJson as ManifestV3Export;
-const viteManifestHackIssue846 = {
-  // Workaround from https://github.com/crxjs/chrome-extension-tools/issues/846#issuecomment-1861880919.
-  name: 'manifestHackIssue846',
-  renderCrxManifest(_manifest, bundle) {
-    bundle['manifest.json'] = bundle['.vite/manifest.json'];
-    bundle['manifest.json'].fileName = 'manifest.json';
-    delete bundle['.vite/manifest.json'];
-  },
-};
 
 // https://vitejs.dev/config/
 export default {
-  plugins: [react(), svgr({ include: '**/*.svg' }), viteManifestHackIssue846, crx({ manifest })],
+  plugins: [react(), svgr({ include: '**/*.svg' }), crx({ manifest })],
   resolve: {
     alias: [
       { find: 'public', replacement: resolve(__dirname, './public') },
